9083427b86bad3b28625086b11a61c94ac267b06
GRAZIELE DA SILVA BARBOSA VASCONCELOS <grazisbvasconcelos@gmail.com>
2019-02-07 19:33:32 -0200

    segunda versao beta

--- comprehend.go
+++ comprehend.go
@@ -1,30 +1,42 @@
 package main
 
 import (
-        "fmt"
-        "github.com/aws/aws-sdk-go/aws"
-        "github.com/aws/aws-sdk-go/aws/session"
-        "github.com/aws/aws-sdk-go/service/comprehend"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/request"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/comprehend"
 )
 
 func main() {
 
-        sess := session.Must(session.NewSessionWithOptions(session.Options{
-                SharedConfigState: session.SharedConfigEnable,
-        }))
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
 
-        client := comprehend.New(sess, &aws.Config{
-                Region: aws.String("us-east-2"),
-        })
+	client := comprehend.New(sess, &aws.Config{
+		Region: aws.String("us-east-2"),
+	})
 
-        // Example sending a request using the DetectEntitiesRequest method.
-        req, resp := client.DetectEntitiesRequest(&comprehend.DetectEntitiesInput{LanguageCode: aws.String("en"), Text: aws.String("Amazon.com, Inc. is located in Seattle, WA and was founded July 5th, 1994 by Jeff Bezos, allowing customers to buy everything from books to blenders. Seattle is north of Portland and south of Vancouver, BC. Other notable Seattle - based companies are Starbucks and Boeing.")})
+	request, response := detectEntity(client)
 
-        err := req.Send()
-        if err == nil { // resp is now filled
-                fmt.Println(resp)
-        } else {
-                fmt.Println(err)
-        }
+	err := request.Send()
 
+	if err == nil { // resp is now filled
+		fmt.Println(response)
+	} else {
+		fmt.Println(err)
+	}
+
+	for _, ent := range response.Entities {
+		fmt.Printf(aws.StringValue(ent.Text) + "\n")
+		//fmt.Printf("%v \n", *ent.Text)
+	}
+
+}
+
+func detectEntity(client *comprehend.Comprehend) (*request.Request, *comprehend.DetectEntitiesOutput) {
+	// Example sending a request using the DetectEntitiesRequest method.
+	return client.DetectEntitiesRequest(&comprehend.DetectEntitiesInput{LanguageCode: aws.String("en"), Text: aws.String("Amazon.com, Inc. is located in Seattle, WA and was founded July 5th, 1994 by Jeff Bezos, allowing customers to buy everything from books to blenders. Seattle is north of Portland and south of Vancouver, BC. Other notable Seattle - based companies are Starbucks and Boeing.")})
 }
