2371ff44640341dab89453aa9d51aa9efed8bcc4
grazielevasconcelos <grazisbvasconcelos@gmail.com>
2019-02-12 01:04:04 -0200

    versao com melhorias

--- comprehend.go
+++ comprehend.go
@@ -1,63 +1,51 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/gorilla/mux"
-	"log"
-	"net/http"
-
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/request"
 	"github.com/aws/aws-sdk-go/service/comprehend"
 )
 
 func main() {
-
-	router := mux.NewRouter()
-	log.Fatal(http.ListenAndServe(":8000", router))
-
-	router.HandleFunc("/comprehend", getAwsComprehend).Methods("GET")
+	getAwsComprehend()
 
 }
 
-func detectEntity(client *comprehend.Comprehend) (*request.Request, *comprehend.DetectEntitiesOutput) {
-	// Example sending a request using the DetectEntitiesRequest method.
-	return client.DetectEntitiesRequest(&comprehend.DetectEntitiesInput{LanguageCode: aws.String("en"), Text: aws.String("Amazon.com, Inc. is located in Seattle, WA and was founded July 5th, 1994 by Jeff Bezos, allowing customers to buy everything from books to blenders. Seattle is north of Portland and south of Vancouver, BC. Other notable Seattle - based companies are Starbucks and Boeing.")})
-}
-
-func getAwsComprehend(w http.ResponseWriter, r *http.Request){
-	sess := session.Must(session.NewSessionWithOptions(session.Options{
-		SharedConfigState: session.SharedConfigEnable,
-	}))
-
-	client := comprehend.New(sess, &aws.Config{
-		Region: aws.String("us-east-2"),
-	})
-
+func getAwsComprehend(){
+	sess := getSession()
+	client := getClient(sess)
 	req, response := detectEntity(client)
-
 	err := req.Send()
 
 	if err == nil { // resp is now filled
-		err := json.NewEncoder(w).Encode(response)
 		fmt.Println(response)
-		if err != nil {
-			http.Error(w, err.Error(), 400)
-			return
-		}
+		getCity(response)
 	} else {
-		err := json.NewEncoder(w).Encode(err)
-		if err != nil {
-			http.Error(w, err.Error(), 400)
-			return
-		}
+		fmt.Println(err)
 	}
+}
 
-	//for _, ent := range response.Entities {
-	//	fmt.Printf(aws.StringValue(ent.Text) + "\n")
-	//	//fmt.Printf("%v \n", *ent.Text)
-	//}
+func getCity(response *comprehend.DetectEntitiesOutput){
+	for _, ent := range response.Entities {
+		fmt.Printf(aws.StringValue(ent.Text) + "\n")
+		//fmt.Printf("%v \n", *ent.Text)
+	}
+}
 
+func getSession() *session.Session{
+	return session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+}
+
+func getClient(sess *session.Session) *comprehend.Comprehend{
+	return comprehend.New(sess, &aws.Config{
+		Region: aws.String("us-east-2"),
+	})
+}
+func detectEntity(client *comprehend.Comprehend) (*request.Request, *comprehend.DetectEntitiesOutput) {
+	// Example sending a request using the DetectEntitiesRequest method.
+	return client.DetectEntitiesRequest(&comprehend.DetectEntitiesInput{LanguageCode: aws.String("en"), Text: aws.String("Amazon.com, Inc. is located in Seattle, WA and was founded July 5th, 1994 by Jeff Bezos, allowing customers to buy everything from books to blenders. Seattle is north of Portland and south of Vancouver, BC. Other notable Seattle - based companies are Starbucks and Boeing.")})
 }
